e3a635c01750173e15cd1e4991e6460bdf3eb99f
hupeng <hupeng@imudges.com>
2016-03-09 12:28:05 +0800

    功能增加

---
+++ src/com/imudges/LoveUApp/listener/Listener.java
@@ -0,0 +1,9 @@
+package com.imudges.LoveUApp.listener;
+
+/**
+ * Created by HUPENG on 2016/3/9.
+ */
+public interface Listener {
+    public void onSuccess();
+    public void onFailure(String msg);
+}
---
+++ src/com/imudges/LoveUApp/service/UserService.java
@@ -0,0 +1,17 @@
+package com.imudges.LoveUApp.service;
+
+import android.content.Context;
+import com.imudges.LoveUApp.listener.Listener;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by HUPENG on 2016/3/9.
+ */
+public class UserService {
+    private RequestParams params;
+    private String reponseStr;
+
+    public void login(String username, String password, Context context, Listener listener){
+
+    }
+}

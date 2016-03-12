b38238f7c1417fdf309a73e90278fd5a0d9ffa67
baiguoqing <1215356195@qq.com>
2016-03-12 18:24:54 +0800

    修订图片处理类 增加调用方法

--- src/com/imudges/LoveUApp/service/PhotoService.java
+++ src/com/imudges/LoveUApp/service/PhotoService.java
@@ -8,104 +8,137 @@ import android.os.Message;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import android.os.StrictMode;
 
 /**
  * Created by dy on 2016/3/9.
  */
 public class PhotoService {
-    //上传的地址
-    private String acceptUrl="";
-    //下载地址
-    private String downPathurl="";
-    Bitmap bitmap;
 
+    private String fileName = "image.jpg";  //报文中的文件名参数
+    private String uploadFile;//+ "/" + fileName;    //待上传的文件路径
+    private String Url="http://183.175.12.157/LOVEU/service/ImageService.php";
     /**
-     * 上传图片
-     * @param pathString
-     */
+     * 上传图片文件至Server的方法
+     * 传参
+     * 1,name
+     * 2,secretkey
+     * 3,图片本地地址Path
+     * 回调图片并获取PAth方法：
+             private ProgressDialog progressDialog;
+             private final String IMAGE_TYPE="image/*";
+             private final int IMAGE_CODE=1;
+             private Bitmap TestBitmap;
+             private String Path;
 
-    public void doUpload(String pathString) {
-        RandomAccessFile raf =  null;
-        try{
-            raf = new RandomAccessFile(pathString, "r");
-            long alllength=raf.length();
-            raf.seek(0);
-            byte[] buffer = new byte[128*1024];//128k
-            int count = 0;
-            while ((count = raf.read(buffer)) != -1){
-                String result = uploadFil(acceptUrl,buffer);
-                System.out.println("MediaActivity doUpload return:"+result+ " count:"+count);
-                break;
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        }finally{
-            try {
-                if(raf!=null)
-                    raf.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-        PhotoService photoService=new PhotoService();
-    }
-    public String uploadFil(String acceptUrl,byte[] data) {
+            private void setImage() {
+                 // TODO Auto-generated method stub
+                //使用intent调用系统提供的相册功能，使用startActivityForResult是为了获取用户选择的图片
+                Intent getAlbum = new Intent(Intent.ACTION_GET_CONTENT);
+                getAlbum.setType(IMAGE_TYPE);
+                startActivityForResult(getAlbum, IMAGE_CODE);
+             }
+             @Override
+             protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+                 // TODO Auto-generated method stub
+                if (resultCode != RESULT_OK) {
+                    Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                super.onActivityResult(requestCode, resultCode, data);
+                ContentResolver resolver = getContentResolver();
+                if (requestCode == IMAGE_CODE) {
+                    try {
+                        Uri originUri = data.getData();
+                        Bitmap bm = MediaStore.Images.Media.getBitmap(resolver, originUri);
+                         TestBitmap = bm;
+                         String[] proj = {MediaStore.Images.Media.DATA};
+                         Cursor cursor = managedQuery(originUri, proj, null, null, null);
+                         int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                         cursor.moveToFirst();
+                         String path = cursor.getString(column_index);
+                         Path =Environment.getExternalStorageDirectory().getPath()+ "/"+path.substring(20);
+                         //Toast.makeText(MainActivity.this, Path, Toast.LENGTH_SHORT).show();
+                         BitmapUtil bitmapUtil = new BitmapUtil(MainActivity.this);
+                         Bitmap myBitmap = bitmapUtil.toRoundBitmap(TestBitmap);
+                         //ImageView userImage=find...;
+                         //userImage.setImageBitmap(myBitmap);
+
+                    } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                         // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+             }
+     */
+    public String uploadFile(String name,String secretkey,String Path){
+        String postUrl=Url+"?"+"UserName="+name+"&SecretKey="+secretkey;
         String end = "\r\n";
         String twoHyphens = "--";
-        String boundary = "******";
+        String boundary = "*****";
         try{
-            URL url = new URL(acceptUrl);
-            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
-            // 设置每次传输的流大小，可以有效防止手机因为内存不足崩溃
-            // 此方法用于在预先不知道内容长度时启用没有进行内部缓冲的 HTTP 请求正文的流。
-            httpURLConnection.setChunkedStreamingMode(data.length);// 128*1024 是128k
-//            允许输入输出流
-            httpURLConnection.setDoInput(true);
-            httpURLConnection.setDoOutput(true);
-            httpURLConnection.setUseCaches(false);
-            // 使用POST方法
-            httpURLConnection.setRequestMethod("POST");
-            httpURLConnection.setRequestProperty("Connection", "Keep-Alive");
-            httpURLConnection.setRequestProperty("Charset", "UTF-8");
-            httpURLConnection.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);//application/octet-stream   multipart/form-data
-            DataOutputStream dos  = new DataOutputStream(httpURLConnection.getOutputStream());
-            dos.writeBytes(twoHyphens + boundary + end);
-            dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\"; filename=\""
-                    // +pathString.substring(pathString.lastIndexOf("/")+1)
-                    +"myfilename"
-                    +"\""
-                    +end);
-            dos.writeBytes(end);
-            dos.write(data,0,data.length);
-            dos.writeBytes(end);
-            dos.writeBytes(twoHyphens + boundary + twoHyphens + end);
-            dos.flush();
-            String reponse = "";
-            if(httpURLConnection.getResponseCode() == 200 ){
-                InputStream is = httpURLConnection.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is,"utf-8");
-                BufferedReader br = new BufferedReader(isr);
-                while (null !=br.readLine()) {
-                    reponse +=br.readLine();
-                }
-                is.close();
+            URL url = new URL(postUrl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            /* Output to the connection. Default is false,
+             set to true because post method must write something to the connection */
+            con.setDoOutput(true);
+            /* Read from the connection. Default is true.*/
+            con.setDoInput(true);
+            /* Post cannot use caches */
+            con.setUseCaches(false);
+            /* Set the post method. Default is GET*/
+            con.setRequestMethod("POST");
+            /* 设置请求属性 */
+            con.setRequestProperty("Connection", "Keep-Alive");
+            con.setRequestProperty("Charset", "UTF-8");
+            con.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+            /*设置StrictMode 否则HTTPURLConnection连接失败，因为这是在主进程中进行网络连接*/
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
+            /* 设置DataOutputStream，getOutputStream中默认调用connect()*/
+            DataOutputStream ds = new DataOutputStream(con.getOutputStream());  //output to the connection
+            ds.writeBytes(twoHyphens + boundary + end);
+            ds.writeBytes("Content-Disposition: form-data; " +"name=\"file\";filename=\"" +fileName + "\"" + end);
+            ds.writeBytes(end);
+            /* 取得文件的FileInputStream */
+            uploadFile=Path;
+            FileInputStream fStream = new FileInputStream(uploadFile);
+            /* 设置每次写入8192bytes */
+            int bufferSize = 8192;
+            byte[] buffer = new byte[bufferSize];   //8k
+            int length = -1;
+            /* 从文件读取数据至缓冲区 */
+            while ((length = fStream.read(buffer)) != -1){
+            	/* 将资料写入DataOutputStream中 */
+                ds.write(buffer, 0, length);
             }
-            System.out.println("MediaActivity uploadFil Reponse:"+reponse);
-            dos.close();
-            return reponse;
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            System.out.println("MediaActivity uploadFil Exception:"+e.getMessage());
+            ds.writeBytes(end);
+            ds.writeBytes(twoHyphens + boundary + twoHyphens + end);
+            /* 关闭流，写入的东西自动生成Http正文*/
+            fStream.close();
+            /* 关闭DataOutputStream */
+            ds.close();
+            /* 从返回的输入流读取响应信息 */
+            InputStream is = con.getInputStream();  //input from the connection 正式建立HTTP连接
+            int ch;
+            StringBuffer b = new StringBuffer();
+            while ((ch = is.read()) != -1){
+                b.append((char) ch);
+            }
+            return "上传成功"+b.toString();
+
+        } catch (Exception e){
+            return e.toString();
         }
-        return "";
     }
 
     /**
-     * 下载图片
+     * 及时显示在线图片
      * 显示图片方法Handler(需放在Activity中与downPhoto函数一起用)
      */
+    Bitmap bitmap;
     Handler handler=new Handler(){
         @Override
         public void handleMessage(Message msg) {
@@ -115,13 +148,13 @@ public class PhotoService {
             }
         }
     };
-    public void downPhoto(){
+    public void downPhoto(String Urldownphoto){
         new Thread(){
             @Override
             public void run() {
                 try {
                     //创建一个url对象
-                    URL url=new URL(downPathurl);
+                    URL url=new URL(Urldownphoto);
                     //打开URL对应的资源输入流
                     InputStream is= url.openStream();
                     //从InputStream流中解析出图片

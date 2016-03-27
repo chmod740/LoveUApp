8aefa39e1b5413590f159e9f940a0c0a70c787d1
caolu <chaolu@imudges.com>
2016-03-27 15:35:10 +0800

    界面+++

--- src/com/imudges/LoveUApp/ui/CooperationFragment/CooperationSecondFragment.java
+++ src/com/imudges/LoveUApp/ui/CooperationFragment/CooperationSecondFragment.java
@@ -1,10 +1,13 @@
 package com.imudges.LoveUApp.ui.CooperationFragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import com.imudges.LoveUApp.ui.R;
 
@@ -13,9 +16,25 @@ import com.imudges.LoveUApp.ui.R;
  */
 public class CooperationSecondFragment extends Fragment{
 
+    private Button bt;
+    private EditText neirong,biaoti;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.cooperation_2, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        bt=(Button) getView().findViewById(R.id.cooperation2_tijiao);
+        neirong = (EditText) getView().findViewById(R.id.cooperation2_neirong);
+        biaoti = (EditText) getView().findViewById(R.id.cooperation2_biaoti);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ;
+            }
+        });
     }
 }
--- src/com/imudges/LoveUApp/ui/CooperationFragment/CooperationThirdFragment2.java
+++ src/com/imudges/LoveUApp/ui/CooperationFragment/CooperationThirdFragment2.java
@@ -1,18 +1,57 @@
 package com.imudges.LoveUApp.ui.CooperationFragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import com.imudges.LoveUApp.ui.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class CooperationThirdFragment2 extends Fragment {
-
+    private ListView listView;
+    private SimpleAdapter simpleAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.cooperation_3_2, container, false);
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listView = (ListView) getView().findViewById(R.id.cooperation3_2_list);
+        simpleAdapter = new SimpleAdapter(getActivity(),
+                getData(),
+                R.layout.item_cooperation_3_1,
+                new String[] { "man","img","info","title"},
+                new int[] { R.id.cooperation3_1_man,R.id.cooperation3_1_img,R.id.cooperation3_1_neirong,R.id.cooperation3_1_biaoti }
+        );
+        listView.setAdapter(simpleAdapter);
+    }
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        int numOfList = 10;
+        Map<String, Object> map;
+        for(int i = numOfList-1; i>=0; i--){
+            int img = R.drawable.default1;
+
+            map = new HashMap<String, Object>();
+            map.put("title",  "标题");
+            map.put("man",  "发出人");
+            map.put("img", img);
+            map.put("info","内容");
+            list.add(map);
+        }
+
+        return list;
     }
 }
